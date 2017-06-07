7576d748a4a66c6e259b37f5d877ad5e0b7f976b
fajarkarim <fajarabdulkarim@gmail.com>
2017-06-07 11:51:56 +0700

    release 1,2,3,4, not tested yet

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 "use strict"
+const db = require('./models')
+const repl = require('repl')
+const replServer = repl.start('>> ')
+
+function create(obj) {
+  db.Student.create(obj)
+  .then(student => {
+    console.log(student.toJSON());
+  })
+  
+}
+
+function getData() {
+  db.Student.getAllData()
+  
+}
+
+replServer.context.create = create
+replServer.context.getData = getData
---
+++ models/student.js
@@ -0,0 +1,87 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Student = sequelize.define('Student', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    height: {
+      type: Sequelize.INTEGER,
+      validate : {
+        min : 150
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        isUnique:function (value, next) {
+          let self = this
+          Student.findOne({
+            where: {email:value},
+          }).then(student => {
+            if (student) {
+              return next('email already in use')
+            } 
+            return next() 
+          })
+          .catch(err => {
+            next(err)
+          })
+        }
+      }
+    },
+    phone: {
+      type: DataTypes.STRING.
+      validate: {
+        isNumeric : {
+          args: true,
+          msg:'Just number'
+        },
+        len : {
+          args: [10,13],
+          msg: 'length must between 10 - 13 character'
+        },
+        isAlphanumeric: {
+          args: false,
+          msg: 'not allow alphanumeric'
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      },
+      getAllData: function () {
+        Student.findAll()
+        .then(students => {
+          students.forEach(student => {
+            console.log(`first name : ${student.first_name}`);
+            console.log(`last name : ${student.last_name}`);
+            console.log(`gender : ${student.gender}`);
+            console.log(`birthday: ${student.birthday}`);
+            console.log(`email : ${student.email}`);
+            console.log(`phone : ${student.phone}`);
+            console.log(`full name : ${student.getFullName()}`);
+            console.log(`\n`);
+          })
+        })
+      }
+    },
+    instanceMethods: {
+      getFullName: function () {
+        return `${this.first_name} ${this.last_name}`
+      },
+      getAge: function () {
+        let now = new Date().getFullYear()
+        let birthday = new Date(this.birthday).getFullYear()
+        let age = now - birthday
+        return `Your age ${age}`
+      }
+    }
+      
+    
+  });
+  return Student;
+};
