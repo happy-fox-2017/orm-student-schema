5599d8e072465be6b45ef4a9baee5eea5f3bb03d
pdvega <pdvegaa@gmail.com>
2017-06-12 08:26:03 +0700

    orm student schema

--- index.js
+++ index.js
@@ -1,98 +1,162 @@
 "use strict"
+const db = require('./models');
+const repl = require('repl');
 
-const db = require ('./models');
-const repl = require ('repl');
-const replStart = repl.start('ACTION >>');
+let replStart = repl.start({
+  prompt: '>>> ',
+  input: process.stdin,
+  output: process.stdout
+});
 
-function getAge(){
-  db.Student
-  .findAll()
+function getAge() {
+  db.Student.findAll()
   .then(Students => {
     Students.forEach(Students => {
       console.log(Students.getAge());
-    })
-  })
+    });
+  });
 }
 
 function getFullName() {
-  db.Student
-  .findAll()
+  db.Student.findAll()
   .then(Students => {
     Students.forEach(Students => {
       console.log(Students.getFullName());
-    })
-  })
+    });
+  });
 }
 
-function getAllData(){
-  db.Student
-  .findAll()
+function getAllData() {
+  db.Student.findAll()
   .then(Students => {
     Students.forEach(Students => {
-      console.log(`Student ID : ${Students.id}`);
-      console.log(`Student Name : ${Students.getFullName()}`);
-      console.log(`Student Gender : ${Students.gender}`);
-      console.log(`Student Birthday : ${Students.birthdate}`);
-      console.log(`Student Email : ${Students.email}`);
-      console.log(`Student Phone Number : ${Students.phone}`);
-      console.log(`Student Height : ${Students.height} cm`);
-      console.log('\n');
+      console.log(`Student ID: ${Students.id}`);
+      console.log(`Student ${Students.getFullName()}`);
+      console.log(`Student gender: ${Students.gender}`);
+      console.log(`Student birthdate: ${Students.birthdate}`);
+      console.log(`Student email: ${Students.email}`);
+      console.log(`Student phone number: ${Students.phone}`);
+      console.log(`\n`);
     })
   })
 }
 
-function addStudent(object) {
+function createData(object) {
   db.Student.create(object)
-  .then(() => {
-    console.log('Data student added');
+  .then( () => {
+    console.log("Data added");
   })
   .catch(err => {
     console.log(err.message);
   })
 }
 
-replStart.context.getAge = getAge
-replStart.context.getFullName = getFullName
-replStart.context.getAllData = getAllData
-replStart.context.addStudent = addStudent 
+replStart.context.getAge = getAge;
+replStart.context.getFullName = getFullName;
+replStart.context.getAllData = getAllData;
+replStart.context.createData= createData;
 
-addStudent({first_name:'Dian', last_name:'Wiseka', gender:'Female', birthdate:'1989-21-01', email:'dianwiseka@yahoo.com', phone:'081315247654'})
+//Testcase
 
 
-// function getNameAndAge() {
+//createData({first_name: "Alvita", last_name: "Vidya Indriyana", gender: "Female", birthday: "1999-08-12", email: "alvita@gmail.com", phone: "1876489287382", height: 145})
+
+
+// "use strict"
+// 
+// const db = require ('./models');
+// const repl = require ('repl');
+// const replStart = repl.start('ACTION >>');
+// 
+// function getAge(){
 //   db.Student
 //   .findAll()
-//   .then(students => {
-//     students.forEach(student => {
-//       console.log(`${student.getFullName()}`);
-//     })
-//   })
-//   .catch(error => {console.log(error);
-//   })
-// }
-
-// function getWashes() {
-//   db.Wash
-//   .findAll()
-//   .then(washes => {
-//     washes.forEach(w => {
-//       console.log(`${w.id} ${w.number} ${w.getPrice()}`);
-//     })
-//   })
-//   .catch(error => { console.log(error) });
-// }
-
-// 
-// 
-// function getAge() {
-//   db.Students.findAll()
 //   .then(Students => {
 //     Students.forEach(Students => {
 //       console.log(Students.getAge());
-//     });
-//   });
+//     })
+//   })
 // }
-
-
-// replStart.context.getNameAndAge = getNameAndAge;
-// replStart.context.getAllData=db.Student.getAllData;
+// 
+// function getFullName() {
+//   db.Student
+//   .findAll()
+//   .then(Students => {
+//     Students.forEach(Students => {
+//       console.log(Students.getFullName());
+//     })
+//   })
+// }
+// 
+// function getAllData(){
+//   db.Student
+//   .findAll()
+//   .then(Students => {
+//     Students.forEach(Students => {
+//       console.log(`Student ID : ${Students.id}`);
+//       console.log(`Student Name : ${Students.getFullName()}`);
+//       console.log(`Student Gender : ${Students.gender}`);
+//       console.log(`Student Birthday : ${Students.birthdate}`);
+//       console.log(`Student Email : ${Students.email}`);
+//       console.log(`Student Phone Number : ${Students.phone}`);
+//       console.log(`Student Height : ${Students.height} cm`);
+//       console.log('\n');
+//     })
+//   })
+// }
+// 
+// function addStudent(object) {
+//   db.Student.create(object)
+//   .then(() => {
+//     console.log('Data student added');
+//   })
+//   .catch(err => {
+//     console.log(err.message);
+//   })
+// }
+// 
+// replStart.context.getAge = getAge
+// replStart.context.getFullName = getFullName
+// replStart.context.getAllData = getAllData
+// replStart.context.addStudent = addStudent 
+// 
+// addStudent({first_name:'Dian', last_name:'Wiseka', gender:'Female', birthdate:'1989-21-01', email:'dianwiseka@yahoo.com', phone:'081315247654'})
+// 
+// 
+// // function getNameAndAge() {
+// //   db.Student
+// //   .findAll()
+// //   .then(students => {
+// //     students.forEach(student => {
+// //       console.log(`${student.getFullName()}`);
+// //     })
+// //   })
+// //   .catch(error => {console.log(error);
+// //   })
+// // }
+// 
+// // function getWashes() {
+// //   db.Wash
+// //   .findAll()
+// //   .then(washes => {
+// //     washes.forEach(w => {
+// //       console.log(`${w.id} ${w.number} ${w.getPrice()}`);
+// //     })
+// //   })
+// //   .catch(error => { console.log(error) });
+// // }
+// 
+// // 
+// // 
+// // function getAge() {
+// //   db.Students.findAll()
+// //   .then(Students => {
+// //     Students.forEach(Students => {
+// //       console.log(Students.getAge());
+// //     });
+// //   });
+// // }
+// 
+// 
+// // replStart.context.getNameAndAge = getNameAndAge;
+// // replStart.context.getAllData=db.Student.getAllData;
--- models/student.js
+++ models/student.js
@@ -1,80 +1,167 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-    var Student = sequelize.define('Student', {
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        gender: DataTypes.STRING,
-        birthdate: DataTypes.DATE,
-        // email : DataTypes.STRING,
-        // phone: DataTypes.STRING
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            len: {
-              args: [6, 128],
-              msg: "Email address must be between 6 and 128 characters"
-            },
-            isEmail: {
-              msg: "Email address must be valid"
-            }
-          }
+  var Student = sequelize.define('Student', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    birthdate: DataTypes.DATE,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Input your email in correct order, use @ duh!"
         },
-        phone: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            isAlphanumeric: {
-              args: false,
-              msg: "Number phone is not allow Alphanumeric"
-            },
-            len: {
-              args: [8, 13],
-              msg: "Number phone must be length 8 until 13"
-            },
-            isNumeric: {
-              args: true,
-              msg: "Number phone is not allow using letter"
-            }
-          }
-        },
-        height:{
-          type: DataTypes.INTEGER,
-          validate : {
-            min:{
-              args : 150,
-              msg : 'Only 150cm above can join to this school'
-            }
-          }
-        }
-      },  {
-          classMethods: {
-            associate: function(models) {
-              // associations can be defined here
-            },
-            getAllData : function(dataAll){
-              Student.findAll()
-              .then(Students => {
-                return dataAll(Students)
-                })
+        isUnique: function(content, next) {
+          Student.find({
+              where: {
+                email: content
+              },
+              attributes: ['id']
+            })
+            .then((Students) => {
+              if (Students && this.id !== Students.id) {
+                return next("Email is someone's precious")
               }
-            },
-            
-    instanceMethods: {
-      getFullName: () => {
-          let fullName = `Fullname : ${this.first_name} ${this.last_name}`;
-          return fullName;
+              return next();
+            })
+            .catch((err) => {
+              return next(err)
+            })
+        } //isUnique
+      } //validate
+    }, //email
+    phone: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [10, 13],
+          msg: "Please input the number between 10 to 13 length"
+        },
+        isNumeric: {
+          args: true,
+          msg: "Please do not input letter in number"
+        },
+        isAlphanumeric: {
+          args: false,
+          msg: "Please do not input alphanumeric in number"
+        }
+      } //validate
+    }, //phone
+    height: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: 150,
+          msg: "Only 150cm above can get to this school unfotunately"
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
       },
+      getAllData: function(callback) {
+        Student.findAll()
+          .then(Students => {
+            return callback(Students)
+          })
+      }
+    },
+
+    instanceMethods: {
+      getFullName: function() {
+        let fullName = `Fullname: ${this.first_name} ${this.last_name}`;
+        return fullName;
+      },
+
       getAge: function() {
-        let result = new Date().getFullYear() - this.birthdate.getFullYear()
-        return `Age this year : ${result}`;
-        // let yearBirthdate = new Date(this.birthdate).getFullYear();
-        // let yearNow = new Date().getFullYear();
-        // return `Age this year : ${yearNow} - ${yearBirthdate}`;
+        let result = new Date().getFullYear() - this.birthdate.getFullYear();
+        return `Age: ${result}`;
       }
     }
   });
   return Student;
 };
+
+// 'use strict';
+// module.exports = function(sequelize, DataTypes) {
+//     var Student = sequelize.define('Student', {
+//         first_name: DataTypes.STRING,
+//         last_name: DataTypes.STRING,
+//         gender: DataTypes.STRING,
+//         birthdate: DataTypes.DATE,
+//         // email : DataTypes.STRING,
+//         // phone: DataTypes.STRING
+//         email: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//           unique: true,
+//           validate: {
+//             len: {
+//               args: [6, 128],
+//               msg: "Email address must be between 6 and 128 characters"
+//             },
+//             isEmail: {
+//               msg: "Email address must be valid"
+//             }
+//           }
+//         },
+//         phone: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//           unique: true,
+//           validate: {
+//             isAlphanumeric: {
+//               args: false,
+//               msg: "Number phone is not allow Alphanumeric"
+//             },
+//             len: {
+//               args: [8, 13],
+//               msg: "Number phone must be length 8 until 13"
+//             },
+//             isNumeric: {
+//               args: true,
+//               msg: "Number phone is not allow using letter"
+//             }
+//           }
+//         },
+//         height:{
+//           type: DataTypes.INTEGER,
+//           validate : {
+//             min:{
+//               args : 150,
+//               msg : 'Only 150cm above can join to this school'
+//             }
+//           }
+//         }
+//       },  {
+//           classMethods: {
+//             associate: function(models) {
+//               // associations can be defined here
+//             },
+//             getAllData : function(dataAll){
+//               Student.findAll()
+//               .then(Students => {
+//                 return dataAll(Students)
+//                 })
+//               }
+//             },
+//             
+//     instanceMethods: {
+//       getFullName: () => {
+//           let fullName = `Fullname : ${this.first_name} ${this.last_name}`;
+//           return fullName;
+//       },
+//       getAge: function() {
+//         let result = new Date().getFullYear() - this.birthdate.getFullYear()
+//         return `Age this year : ${result}`;
+//         // let yearBirthdate = new Date(this.birthdate).getFullYear();
+//         // let yearNow = new Date().getFullYear();
+//         // return `Age this year : ${yearNow} - ${yearBirthdate}`;
+//       }
+//     }
+//   });
+//   return Student;
+// };
